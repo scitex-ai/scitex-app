1838d7945781b955308fcd6f127b7ec39cd5ef40
Yusuke Watanabe <ywatanabe@scitex.ai>
2026-05-02 17:10:57 +0900

    snapshot: pre-rename for -V audit fix

--- src/scitex_app/_cli/_main.py
+++ src/scitex_app/_cli/_main.py
@@ -107,7 +107,7 @@ else:
         context_settings=CONTEXT_SETTINGS,
         invoke_without_command=True,
     )
-    @click.version_option(version=_get_version(), prog_name="scitex-app")
+    @click.version_option(_get_version(), "-V", "--version", prog_name="scitex-app")
     @click.option(
         "--help-recursive", is_flag=True, help="Show help for all subcommands."
     )
